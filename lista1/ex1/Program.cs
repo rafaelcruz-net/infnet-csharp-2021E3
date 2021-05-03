532d2f4883f49a3cf8bb3094ab55a7e6d7299778
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2021-05-03 20:57:27 -0300

    Primeiro Commit csharp

---
+++ lista1/ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
