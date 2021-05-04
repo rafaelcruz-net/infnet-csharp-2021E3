7a72b2d58855892cdcf2726901a376a8127c7b8a
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2021-05-03 21:16:09 -0300

    Finalização do exercicio 2 - lista 1

---
+++ lista1/ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            
+            Console.WriteLine("Informe seu Nome");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Informe sua idade");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade > 18)
+                Console.WriteLine($"{nome} é maior que 18 anos");
+            else
+                Console.WriteLine($"{nome} não é maior que 18 anos");
+        }
+    }
+}
