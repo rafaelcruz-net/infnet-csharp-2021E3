314fb6a99b268f48ef49a2849fd35d0823d6d9de
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2021-05-03 22:04:21 -0300

    Finalização do Exercicio 1 - lista 2

---
+++ lista2/ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INFORME O NUMERO A SER MULTIPLICADO");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+                Console.WriteLine($"{n} x {i} = {n * i}");
+
+
+        }
+    }
+}
