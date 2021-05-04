110cad97ac013d30e6b5f99138ef4a023375501f
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2021-05-03 21:28:59 -0300

    Finalização do Ex3 - Lista 1

---
+++ lista1/ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao pattern de triangulo");
+            Console.WriteLine("Informe quantas linhas deseja imprimir");
+
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+
+            Console.Read();
+
+        }
+    }
+}
