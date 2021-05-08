5fef0435f7d4468b9d4d1869221f0e29ce54ba29
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2021-05-07 22:04:09 -0300

    Correcao Lista 2

---
+++ lista2/ex5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao Caixa Eletronico");
+            Console.WriteLine("Digite o valor do troco");
+            int valor = Convert.ToInt32(Console.ReadLine());
+
+            int[] notas = new int[] { 50, 25, 10, 5, 1};
+
+            int index = 0;
+            int qtNota = 0;
+            string result = "";
+
+            while(valor != 0) 
+            {
+                qtNota = valor / notas[index];
+
+                if (qtNota != 0) 
+                {
+                    result += $"{qtNota} de R$ {notas[index]} \n";
+                    valor = valor % notas[index];
+                }
+
+                index++;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
